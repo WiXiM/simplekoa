1617b06fc49f3c58293343cc025cd6763b4b7c06
wiximix <wiximix@gmail.com>
2018-03-31 15:47:41 +0800

    安装依赖

--- example/index.js
+++ example/index.js
@@ -1,1 +1,12 @@
 let wiKoa = require('../src/application')
+
+let app = new wiKoa()
+
+app.use((req, res) => {
+    res.writeHead(200)
+    res.end('Hi~ WiX')
+})
+
+app.listen(3000, () => {
+    console.log('listen on 3000');
+})
